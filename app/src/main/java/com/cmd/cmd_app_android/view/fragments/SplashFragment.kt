661fa0a7629183a784bca2693e83c35c25bf0616
chriszod <chriszodd@gmail.com>
2021-10-13 13:08:37 +0100

    implemented Splash Screen

---
+++ app/src/main/java/com/cmd/cmd_app_android/view/fragments/SplashFragment.kt
@@ -0,0 +1,44 @@
+package com.cmd.cmd_app_android.view.fragments
+
+import android.os.Bundle
+import android.service.autofill.Validators.or
+import android.view.View
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.cmd.cmd_app_android.view.activities.MainActivity
+import com.cmd.cmd_app_android.view.activities.StarterActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import thecmdteam.cmd_app_android.R
+import thecmdteam.cmd_app_android.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment(R.layout.fragment_splash){
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        lifecycleScope.launchWhenStarted{
+            delay(3000L)
+            findNavController().navigate(R.id.action_splashFragment_to_onBoardingFragment)
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+
+    override fun onStart() {
+        super.onStart()
+        (activity as StarterActivity).window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        or View.SYSTEM_UI_FLAG_FULLSCREEN
+        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+
+    }
+}
